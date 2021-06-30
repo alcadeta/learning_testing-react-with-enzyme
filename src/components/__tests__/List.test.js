da2d70577879f21c898fb6b2d194374ce5b7a3a7
Alican Demirtas <demirtaa@legal.regn.net>
2021-06-30 20:37:39 +0100

    `shallow` calls replaced with `mount` for full rendering.

--- src/components/__tests__/List.test.js
+++ src/components/__tests__/List.test.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { shallow } from "../../enzyme"
+import { mount } from "../../enzyme";
 import List from "../List";
 
 describe("List tests", () => {
     it("renders list-items", () => {
         const items = ["one", "two", "three"];
-        const wrapper = shallow(<List items={items} />);
+        const wrapper = mount(<List items={items} />);
+
+        console.log(wrapper.debug());
 
         // expect the wrapper object to be defined
         expect(wrapper.find(".list-items")).toBeDefined();
@@ -14,15 +16,15 @@ describe("List tests", () => {
 
     it("renders a list item", () => {
         const items = ["foo", "bar"];
-        const wrapper = shallow(<List items={items} />);
+        const wrapper = mount(<List items={items} />);
 
         // expect an element to exist in the component
-        expect(wrapper.contains(<li key="Thor" className="item">Thor</li>)).toBeTruthy();
+        expect(wrapper.contains(<li key="foo" className="item">foo</li>)).toBeTruthy();
     });
 
     it("renders correct text in item", () => {
         const items = ["John", "James", "Luke"];
-        const wrapper = shallow(<List items={items} />);
+        const wrapper = mount(<List items={items} />);
 
         // expect the child of the first item to be an array
         expect(wrapper.find(".item").get(0).props.children).toEqual("John");
